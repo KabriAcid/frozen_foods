736c0d682913397e3e3c37a4e18c663a57d085d7
KabriAcid <kabriacid01@gmail.com>
2025-07-12 14:44:56 +0100

    commit

---
+++ admin/components/header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Peace Logistix - Dashboard</title>
+    <link rel="shortcut icon" href="../favicon.png" type="image/x-icon">
+
+    <!-- Bootstrap 5 JS Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Icons stylesheets -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
+
+    <!-- Creative Bootstrap -->
+    <link rel="stylesheet" href="../assets/css/soft-design-system-pro.min.css">
+
+    <!-- Toasted library -->
+    <link rel="stylesheet" href="../assets/css/toasted.css">
+
+    <!-- Custom stylesheets -->
+    <link rel="stylesheet" href="../assets/css/dashboard.css">
+    <link rel="stylesheet" href="../assets/css/style.css">
+</head>
